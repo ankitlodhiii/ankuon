807f8c0775e47f58438db0b769ae7a91cf770ff3
Ankit Lodhi <ankitlodhiii06@gmail.com>
2026-08-13 22:17:14 +0530

    Serve Investor and Admin on same domain

--- ankuon/urls.py
+++ ankuon/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from app.views import IndexView, DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('api/', include('app.api.urls')),  # For API routes
+
+    # Investor App (AnkuOn2)
+    path('invest/', TemplateView.as_view(template_name='app/investor.html'), name='invest'),
+
+    # Admin Panel
+    path('admin-panel/', TemplateView.as_view(template_name='app/admin_panel.html'), name='admin_panel'),
+
+    path('api/', include('app.api.urls')),
 ]
